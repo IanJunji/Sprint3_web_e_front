86277a0293e8a0e17701a3e9f59b1bb9756d6bc3
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-04-25 18:04:37 -0300

    Fix - Cards

--- Sprint3_WebDev/src/components/sobre.jsx
+++ Sprint3_WebDev/src/components/sobre.jsx
@@ -3,18 +3,19 @@ function Sobre(){
     return(
         <>
         <div className="container1">
-            <h1>Sobre nós</h1>
+            <h1>Nossos Valores</h1>
+            <p>Conheça quem somos e o que nos move a transformar o futuro.</p>
             <div className="cards">
-                <div className="card">
+                <div className="card card-card1">
                     <h1>Segurança</h1>
                 </div>
-                <div className="card">
+                <div className="card card-card2">
                     <h1>Controle</h1>
                 </div>
-                <div className="card">
+                <div className="card card-card3">
                     <h1>Precisão</h1>
                 </div>
-                <div className="card">
+                <div className="card card-card4">
                     <h1>Confiança</h1>
                 </div>
             </div>
