aea145d5a9019b869633aa0b22ade254e9e705d5
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-06-13 20:50:28 -0300

    Feat - Dashboard

--- Sprint3_WebDev/src/App.jsx
+++ Sprint3_WebDev/src/App.jsx
@@ -1,16 +1,24 @@
-
-import './App.css'
-
-
+import { StrictMode } from "react";
+import Header from "./components/header";
+import Sobre from "./components/sobre";
+import Footer from "./components/Footer";
+import Contato from "./components/contato";
+import Form from "./components/form";
+import Banner from "./components/banner";
 
 function App() {
-
-
   return (
     <>
-      
+      <StrictMode>
+        <Header />
+        <Sobre />
+        <Banner />
+        <Contato />
+        <Form />
+        <Footer />
+      </StrictMode>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Sprint3_WebDev/src/main.jsx
+++ Sprint3_WebDev/src/main.jsx
@@ -1,20 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './components/header'
-import Sobre from './components/sobre'
-import Footer from './components/Footer'
-import Contato from './components/contato'
-import Form from './components/form'
-import Banner from './components/banner'
+import App from './App'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <Sobre />
-    <Banner />
-    <Contato />
-    <Form />
-    <Footer />
+    <App />
   </StrictMode>
 )
