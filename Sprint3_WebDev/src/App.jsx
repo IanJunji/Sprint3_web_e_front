f8f34448dacf0616a077da3e64d27e16877490fa
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-06-13 21:56:29 -0300

    Fix - Dashboard style

--- Sprint3_WebDev/src/App.jsx
+++ Sprint3_WebDev/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
 import Register from './components/Register';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/dashboard';
 import Sobre from "./components/sobre";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
--- Sprint3_WebDev/src/components/dashboard.jsx
+++ Sprint3_WebDev/src/components/dashboard.jsx
@@ -1,4 +1,4 @@
-import '../styles/dashboard.css'
+import '../styles/dashboard.scss'
 
 function Dashboard(){
     return(
