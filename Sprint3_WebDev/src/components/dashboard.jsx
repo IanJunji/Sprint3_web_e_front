2677404b6289d49fc31e7a30f9a0b46a77c61313
DevCilas <160146962+DevCilas@users.noreply.github.com>
2025-06-13 21:25:42 -0300

    Fix - Dashboard

--- Sprint3_WebDev/src/components/dashboard.jsx
+++ Sprint3_WebDev/src/components/dashboard.jsx
@@ -1,29 +1,63 @@
+import '../styles/dashboard.css'
 function Dashboard(){
     return(
         <>
         <div className="container">
             <div className="title">
-                <h1>Painel de Controle</h1>
+                <h1>Dashboard de Controle</h1>
             </div>
             <div className="quadro">
                 <div className="nav">
-                    <label for="model-select">Escolha um modelo:</label>
-
-                    <select name="models" id="model-select">
-                    <optgroup label="GEMINI 2.5">
-                        <option value="gemini-2.5-pro-preview-06-05">Gemini 2.5 Pro Preview</option>
+                    
+                    <label htmlFor="local-select">Local: </label>
+                    <select name="models" id="local-select">
+                    <optgroup>
+                        <option value="">Ambulatório 1</option>
                     </optgroup>
-                    <optgroup label="GEMINI 2.0">
-                        <option value="gemini-2.5-pro-preview-05-06">Gemini 2.5 Pro Preview 05-06</option>
+                    <optgroup>
+                        <option value="gemini-2.5-pro-preview-05-06">Ambulatório 2</option>
                     </optgroup>
-                    <optgroup label="GEMMA">
-                        
+                    <optgroup>
+                        <option value="gemini-2.5-pro-preview-05-06">Ambulatório 3</option>
                     </optgroup>
-                    <optgroup label="OTHER">
-                        
-                    </optgroup>
+                    
                     </select>
+                     
+                     <label htmlFor="cam-select">Câmera: </label>
+                    <select name="models" id="cam-select">
+                    <optgroup>
+                        <option value="">Câmera 1</option>
+                    </optgroup>
+                    <optgroup>
+                        <option value="gemini-2.5-pro-preview-05-06">Câmera 2</option>
+                    </optgroup>
+                    <optgroup>
+                        <option value="gemini-2.5-pro-preview-05-06">Câmera 3</option>
+                    </optgroup>
+                
+                    </select>
+
+                     <label htmlFor="medicine-select">Medicamento: </label>
+                    <select name="models" id="medicine-select">
+                    <optgroup>
+                        <option value="">Dorflex</option>
+                    </optgroup>
+                    <optgroup>
+                        <option value="gemini-2.5-pro-preview-05-06">Dipirona</option>
+                    </optgroup>
+                    <optgroup>
+                        <option value="gemini-2.5-pro-preview-05-06">Buscopan</option>
+                    </optgroup>
+                
+                    </select>
+
+                    
+
+                    
                 </div>
+                <div className="result">
+                        
+                    </div>
             </div>
         </div>
         </>
